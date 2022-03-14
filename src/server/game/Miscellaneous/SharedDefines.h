9cd4050e706ced643a948932da0d90fd0e6c5678
Nehyren <100253735+Nehyren@users.noreply.github.com>
2022-03-14 18:01:50 +0100

    Update SharedDefines.h

--- src/server/game/Miscellaneous/SharedDefines.h
+++ src/server/game/Miscellaneous/SharedDefines.h
@@ -66,8 +66,8 @@ enum Races
     RACE_WORGEN             = 12,
     RACE_PANDAREN_HORDE     = 13,
     RACE_PANDAREN_ALLIANCE  = 14,
-    RACE_NIGHTBORNE         = 15,
-    RACE_VOID_ELF           = 16,
+    RACE_VOID_ELF           = 15,
+    RACE_NIGHTBORNE         = 16,
     RACE_VULPERA            = 17
     //RACE_FOREST_TROLL       = 18,
     //RACE_TAUNKA             = 19,
@@ -83,13 +83,13 @@ enum Races
     (1<<(RACE_NIGHTELF-1))|(1<<(RACE_UNDEAD_PLAYER-1))|(1<<(RACE_TAUREN-1))  | \
     (1<<(RACE_GNOME-1))   |(1<<(RACE_TROLL-1))        |(1<<(RACE_BLOODELF-1))| \
     (1<<(RACE_DRAENEI-1)) |(1<<(RACE_GOBLIN-1))       |(1<<(RACE_WORGEN-1)) | \
-    (1<<(RACE_PANDAREN_HORDE-1)) | (1<<(RACE_PANDAREN_ALLIANCE-1)) | (1<<(RACE_NIGHTBORNE-1)) | \
-    (1<<(RACE_VOID_ELF-1)) | (1<<(RACE_VULPERA-1)))
+    (1<<(RACE_PANDAREN_HORDE-1)) | (1<<(RACE_PANDAREN_ALLIANCE-1))) | (1<<(RACE_VOID_ELF-1)) | \
+    (1<<(RACE_NIGHTBORNE-1)) | (1<<(RACE_VULPERA-1)))
 
 #define RACEMASK_ALLIANCE \
     ((1<<(RACE_HUMAN-1)) | (1<<(RACE_DWARF-1)) | (1<<(RACE_NIGHTELF-1)) | \
     (1<<(RACE_GNOME-1)) | (1<<(RACE_DRAENEI-1)) | (1<<(RACE_WORGEN-1)) | \
-    (1<<(RACE_PANDAREN_ALLIANCE-1)) | (1<<(RACE_VOID_ELF-1)))
+    (1<<(RACE_PANDAREN_ALLIANCE-1))) | (1<<(RACE_VOID_ELF-1)))
 
 #define RACEMASK_HORDE RACEMASK_ALL_PLAYABLE & ~RACEMASK_ALLIANCE
 
